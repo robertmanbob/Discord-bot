34537853c9e6264416f286a1f1ce9db27d4000ef
Robert Vincent <vincerob@oregonstate.edu>
2023-03-19 15:35:42 -0700

    utility scripts placeholder

---
+++ utility.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import configparser
+import time
+from discord import app_commands
+
+rank = {
+    "":1
+}
+
+def role_has_permissions(ctx, role):
+    pass
