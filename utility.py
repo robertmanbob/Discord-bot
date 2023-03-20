9905248f4f49c795a6cd70af9e00fd5c9c101ebb
Robert Vincent <vincerob@oregonstate.edu>
2023-03-19 19:36:09 -0700

    helper functions

--- utility.py
+++ utility.py
@@ -8,5 +8,13 @@ rank = {
     "":1
 }
 
-def role_has_permissions(ctx, role):
-    pass
+def role_rank(role: int):
+    """Returns the integer rank of a role from 1-9, higher is better"""
+    if str(role) in rank:
+        return rank[role]
+    else:
+        return 0
+    
+def role_at_least(role: int, requirement: int):
+    """Returns true if a role meets the requirement, false otherwise"""
+    return role_rank(role) >= requirement
