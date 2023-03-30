ae916d01a96afd134181170c9b14ca02b42221cc
Robert Vincent <vincerob@oregonstate.edu>
2023-03-30 12:07:41 -0700

    Tweaking, outline

--- utility.py
+++ utility.py
@@ -67,18 +67,27 @@ def generate_welcome_card(avatar_url: str, name: str) -> str:
     avatar = Image.open(requests.get(avatar_url, stream=True).raw).convert("RGBA")
     welcome_gif = Image.open("welcome.gif")
 
-    # Create same size alpha layer with circle, gaussian blur, and apply to avatar
+    # Create same size alpha layer with circle and apply to avatar
     alpha = Image.new("L", avatar.size, 0)
     draw = ImageDraw.Draw(alpha)
     draw.ellipse([(0, 0), avatar.size], fill=255)
     avatar.putalpha(alpha)
 
-    avatar = avatar.resize((120, 120))
+    # Create a new image slightly larger than the avatar, make a white outline for the avatar, paste the avatar
+    x = round(avatar.size[0]*1.02)
+    y = round(avatar.size[1]*1.02)
+    outline = Image.new("RGBA", (x, y), 0)
+    draw = ImageDraw.Draw(outline)
+    draw.ellipse([(0, 0), (x, y)], fill="white")
+
+    outline.alpha_composite(avatar, (round((x-avatar.size[0])/2), round((y-avatar.size[1])/2)))
+    outline = outline.resize((120, 120))
+    
 
     # create a banner image that is just a transparent image
     banner = Image.new("RGBA", (500, 140), 0)
     overlay = Image.new("RGBA", banner.size, 0)
-    overlay.paste(avatar, (10, 10))
+    overlay.paste(outline, (10, 10))
     banner.alpha_composite(overlay)
     draw = ImageDraw.Draw(banner)
 
@@ -92,10 +101,10 @@ def generate_welcome_card(avatar_url: str, name: str) -> str:
     textimg = Image.new("RGBA", (width, width), 0)
     textdraw = ImageDraw.Draw(textimg)
     textdraw.text((width//2, width//2), text_in_image, font=font, anchor="mm", fill="LightGrey")
-    textimg = textimg.resize((375, 375), resample=Image.LANCZOS)
+    textimg = textimg.resize((365, 365), resample=Image.LANCZOS)
 
     # Paste the text into the banner
-    banner.paste(textimg, (135, -115))
+    banner.paste(textimg, (135, -110))
     
     gen_welcome = []
     # Open each frame of the welcome gif, paste the banner, save to a new frame
